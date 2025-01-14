fe6bcfe00680657e2c26fc839f6c4ef4227efbc4
imzero238 <nayoung8142@gmail.com>
2025-01-15 00:52:13 +0900

    Initial commit

---
+++ src/main/java/com/nayoung/exchangerateopenapitest/ExchangeRateOpenApiTestApplication.java
@@ -0,0 +1,13 @@
+package com.nayoung.exchangerateopenapitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRateOpenApiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeRateOpenApiTestApplication.class, args);
+	}
+
+}

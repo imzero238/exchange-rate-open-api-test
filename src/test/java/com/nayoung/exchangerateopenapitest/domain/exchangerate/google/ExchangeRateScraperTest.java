4971b0b77e7e66cc361a276c6039e89b51067193
imzero238 <nayoung8142@gmail.com>
2025-01-19 00:22:49 +0900

    build: h2 test db 설정

--- src/test/java/com/nayoung/exchangerateopenapitest/domain/exchangerate/google/ExchangeRateScraperTest.java
+++ src/test/java/com/nayoung/exchangerateopenapitest/domain/exchangerate/google/ExchangeRateScraperTest.java
@@ -5,12 +5,14 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ExchangeRateScraperTest {
 
 	@Autowired
